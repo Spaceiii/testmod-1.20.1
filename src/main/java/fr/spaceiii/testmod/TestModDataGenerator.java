5693ce65cbf2122bf2c5175f65aa7605cc80c4fe
Spaceiii <timothee01meyer@gmail.com>
2025-03-27 00:17:14 +0100

    first commit

---
+++ src/main/java/fr/spaceiii/testmod/TestModDataGenerator.java
@@ -0,0 +1,11 @@
+package fr.spaceiii.testmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TestModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
