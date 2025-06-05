2f16d9e5fff88c971317ede33dae3c7630c26318
Spaceiii <timothee01meyer@gmail.com>
2025-06-05 16:07:32 +0200

    Adding LegendaryBow

---
+++ src/main/java/fr/spaceiii/testmod/items/ModItems.java
@@ -0,0 +1,30 @@
+package fr.spaceiii.testmod.items;
+
+import fr.spaceiii.testmod.TestMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
+
+public class ModItems {
+    public static final Item LEGENDARY_BOW = registerItem("legendary_bow", new LegendaryBow(new FabricItemSettings().rarity(Rarity.EPIC)));
+
+    private static void addItemsToTab(FabricItemGroupEntries entries) {
+        entries.add(LEGENDARY_BOW);
+    }
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(TestMod.MOD_ID, name), item);
+    }
+
+    public static void register() {
+        TestMod.LOGGER.info("Registering mod items for " + TestMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(ModItems::addItemsToTab);
+    }
+}
